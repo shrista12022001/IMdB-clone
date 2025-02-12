6778af6117ab84d0d0211fea87e5aa59fd5bb5a8
ShristaSingh <Shrista.singh@talencred.com>
2025-02-12 16:27:17 +0530

    fetching tmdb api

--- imdb-next/src/app/about/page.jsx
+++ imdb-next/src/app/about/page.jsx
@@ -11,13 +11,13 @@ export default function About() {
   <br/>
   
         <p>
-          Our website is designed to provide you with a comprehensive database of
-          movies from all around the world, along with the latest news, reviews,
-          and trailers. Our movie database is constantly updated with new
-          releases, ensuring that you have access to the latest and greatest in
-          the world of cinema. You can search for movies by title, director,
-          actor, genre, or release date, making it easy to find the perfect movie
-          for any occasion.
+            Our website is designed to provide you with a comprehensive database of
+            movies from all around the world, along with the latest news, reviews,
+            and trailers. Our movie database is constantly updated with new
+            releases, ensuring that you have access to the latest and greatest in
+            the world of cinema. You can search for movies by title, director,
+            actor, genre, or release date, making it easy to find the perfect movie
+            for any occasion.
         </p>
         <br/>
         <br/>
--- imdb-next/src/app/page.js
+++ imdb-next/src/app/page.js
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client"; // Forces client-side rendering
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import Results from "@/components/Results";
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre") || "fetchTrending";
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (!API_KEY) {
+        console.error("API Key is missing. Check your .env.local file.");
+        return;
+      }
+
+      const res = await fetch(
+        `https://api.themoviedb.org/3${
+          genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
+        }?api_key=${API_KEY}&language=en-US&page=1`
+      );
+
+      if (!res.ok) {
+        console.error("Failed to fetch data");
+        return;
+      }
+
+      const data = await res.json();
+      setResults(data.results);
+    }
+
+    fetchData();
+  }, [genre]);
+
   return (
     <div>
-      Home
+      <Results results={results} />
     </div>
-  )
+  );
 }
