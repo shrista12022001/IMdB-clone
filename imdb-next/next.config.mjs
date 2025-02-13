7bd3fe7e0bab80eba2d320b3946dd3e8711e395d
ShristaSingh <Shrista.singh@talencred.com>
2025-02-13 15:00:02 +0530

    add card component

--- imdb-next/next.config.mjs
+++ imdb-next/next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
     env: {
       NEXT_PUBLIC_API_KEY: process.env.API_KEY,
     },
+    images: {
+      domains: ['res.cloudinary.com'],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "image.tmdb.org",
+          pathname:'**',
+        },
+      ],
+    }
   };
   
   export default nextConfig;
